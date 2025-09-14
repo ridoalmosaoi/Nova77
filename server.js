82b9f255bc80d858bbb96f411a38c42b05407dfe
ridoalmosaoi <alsadre65@gmail.com>
2025-09-14 04:23:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
